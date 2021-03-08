0b14d5288efe43aec420b8b343c0418325f77b32
‪Amit Hajaj‬‏ <57085762+hajajon@users.noreply.github.com>
2021-03-08 08:52:26 +0200

    Add files via upload

---
+++ snowman-a/snowman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "snowman.hpp"
+#include <string>
+using namespace std;
+
+namespace ariel{
+    std::string snowman(int num){
+        return "This is snowman.\n";
+    }
+}
+
+// int main()
+// {
+//     return 0;
+// }
