1fc55954fd410d2a3dc47a22b746d34d54810e1a
‪Amit Hajaj‬‏ <57085762+hajajon@users.noreply.github.com>
2021-03-08 08:51:27 +0200

    Delete snowman-a directory

--- snowman-a/snowman-a/ex.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <stdexcept>
-using namespace std;
-
-int main(){
-    cout << "\"  \"" << endl;
-}
--- snowman-a/snowman-a/snowman.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <stdexcept>
-#include <string>
-using namespace std;
-
-namespace ariel{
-    std::string snowman(int num);
-}
